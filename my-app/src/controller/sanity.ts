31eb8aedb2e95842a1e948940a665d70b0860842
xillians <sandra.toftemo@payex.com>
2023-04-02 19:09:41 +0200

    remove key

--- my-app/src/controller/sanity.ts
+++ my-app/src/controller/sanity.ts
@@ -6,7 +6,6 @@ export const client = createClient({
     dataset: "production",
     apiVersion: "2021-10-21",
     useCdn: false,
-    token: 'skxPgWyAwP457vC8QbDMBchfJDX5ecrK1PUeaEyJ4cCD47WxUD7yAPHMhOrWzJXsVxbovNFVb21WjSi4xgAQQKRG9yWH1TwEw7sTYJ5F1u8uoWDt7zn99Q3H8Uli9Uwrl6BrAUMU4lBm2fyhPOw7O2uwfmboeYPfu3ksMx3feqVgfqexYicd'
 });
 
 
